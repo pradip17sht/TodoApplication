e6d252c6ae57d6e46f886b685c5cbe24f0f72cf7
Pradip Shrestha <pradip.shrestha@amniltech.com>
2022-05-26 14:21:09 +0545

    mbl api fetch.

---
+++ TodoApplication/Controllers/MBLController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TodoApplication.Model;
+
+namespace TodoApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MBLController : ControllerBase
+    {
+        public async Task<IActionResult> Index()
+        {
+            MBL mbl = new MBL();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("https://www.machbank.com/page/filter_branch/");
+                httpClient.DefaultRequestHeaders.Clear();
+                HttpResponseMessage response = await httpClient.GetAsync("");
+                if (response.IsSuccessStatusCode)
+                {
+                    var mblResponse = await response.Content.ReadAsStringAsync();
+                    mbl = JsonConvert.DeserializeObject<MBL>(mblResponse);
+                }
+                return Ok(mbl);
+            }
+        }
+    }
+}
---
+++ TodoApplication/Model/MBL.cs
@@ -0,0 +1,43 @@
+﻿namespace TodoApplication.Model
+{
+    public class MBL
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string api_id { get; set; }
+        public string title_np { get; set; }
+        public string type { get; set; }
+        public string code { get; set; }
+        public string code_id { get; set; }
+        public string detail { get; set; }
+        public string detail_np { get; set; }
+        public string manager_name { get; set; }
+        public string manager_name_np { get; set; }
+        public string address { get; set; }
+        public string address_np { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string email_detail { get; set; }
+        public string region { get; set; }
+        public string is_regional_office { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string status { get; set; }
+        public string category { get; set; }
+        public string fax { get; set; }
+        public string district_id { get; set; }
+        public string province { get; set; }
+        public string order { get; set; }
+        public string created_on { get; set; }
+        public string created_by { get; set; }
+        public string updated_on { get; set; }
+        public string updated_by { get; set; }
+        public string hide_in_website { get; set; }
+        public string api_branch_code { get; set; }
+        public string api_title { get; set; }
+        public string group_email { get; set; }
+        public string plus_code { get; set; }
+        public string sol_id { get; set; }
+        public string lang { get; set; }
+    }
+}
